1b04d8bf0e25bbf4f80bab800bfcf9e3b120f5bb
zhouyajun <zyj866955@163.com>
2018-08-05 22:48:52 +0800

    add stack

--- src/main/java/linked/LinkedList.java
+++ src/main/java/linked/LinkedList.java
@@ -17,6 +17,11 @@ public class LinkedList<E> {
         public Node() {
             this(null, null);
         }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
     }
 
     private Node head;
@@ -25,6 +30,14 @@ public class LinkedList<E> {
     public LinkedList() {
         head = null;
         size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
     }
 
     public void addFirst(E e) {
@@ -44,7 +57,7 @@ public class LinkedList<E> {
 
         // 插入位置的前一个节点，从dumpyHead开始遍历
         Node pre = dumpyHead;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < index; i++) {
             pre = pre.next;
         }
 //            Node node = new Node(e);
@@ -69,4 +82,63 @@ public class LinkedList<E> {
         return cur.e;
     }
 
+    public boolean contain(E e) {
+        Node cur = head;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+//        Node cur = head;
+//        while (cur != null) {
+//            res.append(cur + "->");
+//            cur = cur.next;
+//        }
+        for (Node cur = head; cur != null; cur = cur.next) {
+            res.append(cur + "->");
+        }
+        res.append("NUll");
+        return res.toString();
+    }
+
+    public E remove(int index) {
+        Node dumpyhead = new Node(null, null);
+        dumpyhead.next = head;
+        Node pre = dumpyhead;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        Node delNode = pre.next;
+        pre.next = delNode.next;
+        delNode.next = null;
+        size--;
+        return delNode.e;
+    }
+
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(44, 3);
+        System.out.println(linkedList);
+        System.out.println(linkedList.removeFirst());
+        System.out.println(linkedList);
+    }
 }
---
+++ src/main/java/stack/LinkedListStack.java
@@ -0,0 +1,53 @@
+package stack;
+
+import linked.LinkedList;
+
+public class LinkedListStack<E> implements Stack<E> {
+
+    private LinkedList<E> linkedList;
+
+    public LinkedListStack() {
+        linkedList = new LinkedList<E>();
+
+    }
+
+    public int getSize() {
+        return linkedList.getSize();
+    }
+
+    public boolean isEmpty() {
+        return linkedList.isEmpty();
+    }
+
+    public void push(E e) {
+        linkedList.addFirst(e);
+    }
+
+    public E pop() {
+        return linkedList.removeLast();
+    }
+
+    public E peek() {
+        return linkedList.get(0);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: top");
+        res.append(linkedList);
+        return res.toString();
+    }
+
+    public static void main(String[] args){
+        LinkedListStack<Integer> linkedList = new LinkedListStack<Integer>();
+        for (int i = 0; i < 10; i++) {
+            linkedList.push(i);
+            System.out.println(linkedList);
+        }
+        linkedList.push(44);
+        System.out.println(linkedList);
+        linkedList.pop();
+        System.out.println(linkedList);
+    }
+}
