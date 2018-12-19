a87bd2fb5218df596c5f4a0807e1c81e5e342f7c
zhouyajun <zyj665582>
2018-12-19 20:03:11 +0800

    二分搜索

--- src/main/java/algorithm/Solution.java
+++ src/main/java/algorithm/Solution.java
@@ -2,6 +2,7 @@ package algorithm;
 
 import java.util.HashMap;
 
+
 /**
  * Solution
  */
@@ -61,7 +62,14 @@ public class Solution {
     private int binaryResearch(int[] nums, int target) {
         int left = 0, right = 0;
         while (left < right) {
-
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
         }
         return -1;
     }
@@ -80,7 +88,7 @@ public class Solution {
                 right = mid;
             }
         }
-
+        int targetPosition = binaryResearch(nums, target);
         return 1;
     }
 
