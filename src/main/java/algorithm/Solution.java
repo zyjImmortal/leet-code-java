c9ae9966a7b31fd5cf3f0712d34aa030159f355d
zhouyajun <zyj665582>
2018-12-19 17:56:20 +0800

    longestConsecutive

--- src/main/java/algorithm/Solution.java
+++ src/main/java/algorithm/Solution.java
@@ -1,12 +1,66 @@
 package algorithm;
 
+import java.util.HashMap;
+
 /**
  * Solution
  */
 public class Solution {
 
-    public int maxProfit(int[] prices) {
+    private HashMap<Integer, Integer> map = new HashMap<>();
+    private HashMap<Integer, Integer> count = new HashMap<>();
+    private int res = 1;
 
-        return 0;
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            if (map.containsKey(cur)) {
+                continue;
+            }
+            map.put(cur, cur);
+            count.put(cur, 1);
+            if (map.containsKey(cur - 1)) {
+                union(cur, cur - 1);
+            }
+            if (map.containsKey(cur + 1)) {
+                union(cur, cur + 1);
+            }
+        }
+        return res;
+    }
+
+    private int find(int p) {
+        if (p == map.get(p))
+            return p;
+        int parent = find(map.get(p));
+        map.put(p, parent);
+        return parent;
+    }
+
+    private void union(int p, int q) {
+        int px = find(p);
+        int qx = find(q);
+        int rawCount = count.get(px) + count.get(qx);
+        if (count.get(px) < count.get(qx)) {
+            map.put(px, qx); // 将px指向qx
+            count.put(qx, rawCount);
+        } else {
+            map.put(qx, px);
+            count.put(px, rawCount);
+        }
+        res = Math.max(res, rawCount);
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 100, 4, 200, 1, 3, 2 };
+        Solution solution = new Solution();
+        System.out.println(solution.longestConsecutive(nums));
     }
 }
--- src/main/java/arrays/Solution.java
+++ src/main/java/arrays/Solution.java
@@ -3,6 +3,7 @@ package arrays;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -10,18 +11,25 @@ import org.apache.commons.lang3.ArrayUtils;
 
 public class Solution {
 
+    private HashMap<Integer, Integer> map = new HashMap<>(); // map存储每个数的连同节点即父节点
+    private HashMap<Integer, Integer> count = new HashMap<>(); // count存储每个连同的大小，key为标识节点，value为连同中的节点数
+    private int res = 1;
+
     /**
      * 给定一个包含 n 个整数的数组 numss，判断 numss 中是否存在三个元素 a，b，c ，使得 a + b + c
      *  = 0
      * ？找出所有满足条件且不重复的三元组。 注意：答案中不可以包含重复的三元组。 例如, 给定数组 numss =
      * 
-     *  [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+     * 
+     * 
+     * [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+     * 
      * 
      * 
      *
      * 思路： 排序后数组 [-4,-1,-1,0,1,2
      * ,对原数组操作会出现，[-1,0,1], [-1, 1,0]这种情况，但是这是两种情况是相
-     * 的 排序后，两个-1 挨着，0、1就只会被利
+     *  的 排序后，两个-1 挨着，0、1就只会被利
      *  一次
      *
      * 下面这种解法时间复杂度O(n3)太高了
@@ -32,6 +40,7 @@ public class Solution {
     public List<List<Integer>> threeSum(int[] numss) {
         Arrays.sort(numss); // 让后面可能出现满足条件的数字排除掉，比如说[-1,0,1], [-1, 1,0]
                             // 
+                            //
                             //
         HashSet<List<Integer>> array = new HashSet<>();
         for (int i = 0; i < numss.length - 2; i++) {
@@ -153,33 +162,52 @@ public class Solution {
         }
     }
 
-    public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) {
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+
+    public int longestConsecutive(int[] a) {
+        if (a.length == 0) {
             return 0;
         }
-        if (nums.length == 1) {
-            return 1;
-        }
-        HashSet<Integer> hs = new HashSet<>();
-        
-        Arrays.sort(nums);
-        int start = 0, cur = 0, end = 1;
-        int numsLength = nums.length;
-        int maxLength = 1;
-        while (end < numsLength) {
-            if ((nums[end] - nums[cur]) == 1) {
-                // maxLength ++;
-                cur++;
-                end++;
-            } else {
-                start = cur = end;
-                end++;
+        for (int i = 0; i < a.length; i++) {
+            int cur = a[i];
+            if (map.containsKey(cur)) {
+                continue;
             }
-            if ((cur - start + 1) > maxLength) {
-                maxLength = cur - start + 1;
+            map.put(cur, cur);
+            count.put(cur, 1);
+            if (map.containsKey(cur - 1)) {
+                union(cur, cur - 1);
+            }
+            if (map.containsKey(cur + 1)) {
+                union(cur, cur + 1);
             }
         }
-        return maxLength;
+        return res;
+    }
+
+    private int find(int p) {
+        if (p == map.get(p))
+            return p; // 如果某个点的值等于自己，那就是标识节点，满足并查集的自反性
+        int parent = find(map.get(p)); // 递归查找，一级级往上查找
+        map.put(p, parent); // 路径压缩，将每个节点的父节点都指向标识节点，这样在以后的查找中很省时
+        return parent;
+    }
+
+    private void union(int p, int q) {
+        int px = find(p);
+        int qx = find(q);
+        int rawCount = count.get(px) + count.get(qx);
+        if (count.get(px) < count.get(qx)) {
+            map.put(px, qx); // 将px指向qx
+            count.put(qx, rawCount);
+        } else {
+            map.put(qx, px);
+            count.put(qx, rawCount);
+        }
+        res = Math.max(res, rawCount);
     }
 
     public static void main(String[] args) {
